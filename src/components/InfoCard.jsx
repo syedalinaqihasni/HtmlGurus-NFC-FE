2d3e44022f56022ee87578a863f3a84aad7e01c6
espark-mannan <mannan@esparkconsultants.com>
2025-08-01 05:07:36 +0500

    update dashboard data with API

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 
-import { Box, Card, Typography } from "@mui/material";
+import { Box, Card, Skeleton, Typography } from "@mui/material";
 
 import {
   cardContainer,
@@ -19,11 +19,31 @@ const InfoCard = ({
   path,
   bg = "rgba(38, 132, 252, 0.15)",
   valueBg = "rgba(38, 132, 252, 0.08)",
+  loading = false,
 }) => {
   const navigate = useNavigate();
 
+  if (loading) {
+    return (
+      <Card elevation={2} sx={cardContainer(loading)}>
+        <Box sx={{ width: "100%", height: "100%" }}>
+          <Skeleton
+            variant="rectangular"
+            width="100%"
+            height="100%"
+            sx={{ borderRadius: 1 }}
+          />
+        </Box>
+      </Card>
+    );
+  }
+
   return (
-    <Card elevation={2} sx={cardContainer} onClick={() => navigate(path)}>
+    <Card
+      elevation={2}
+      sx={cardContainer(loading)}
+      onClick={() => navigate(path)}
+    >
       {/* Left Column */}
       <Box sx={leftBoxContainer}>
         <Box
@@ -36,10 +56,13 @@ const InfoCard = ({
           <img src={icon} />
         </Box>
 
-        <Typography variant="h4">{heading1}</Typography>
-        {/*  <Typography variant="subtitle2" sx={leftBoxSubtitle}>
+        <Typography variant="h4" color="#1C1C1E" lineHeight={"100%"}>
+          {heading1}
+        </Typography>
+
+        <Typography variant="subtitle2" sx={leftBoxSubtitle}>
           {heading2}
-        </Typography> */}
+        </Typography>
       </Box>
 
       {/* Right Column */}
